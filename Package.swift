03faec17996e88c6b4434b79c805ad24ce8574ff
Logan Sease <logan+sease@team.casa>
2024-06-26 12:03:58 -0400

    use include dir

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     targets: [
         .target(
             name: "QwikJson",
-            path: "Pod/Classes"
+            path: "Pod/Classes",
+            publicHeadersPath: "Pod/Classes"
         )
     ]
 )
